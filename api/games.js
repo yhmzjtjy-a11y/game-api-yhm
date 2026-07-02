fad20044d74683871e9b92f7277a084404b3ab13
yhmzjtjy-a11y <yhmzjtjy@qq.com>
2026-07-02 10:42:00 +0800

    Update games.js

--- api/games.js
+++ api/games.js
@@ -1,12 +1,5 @@
-import { readFile } from "fs/promises";
-
-export default async function handler(req, res) {
-  try {
-    const data = await readFile("./game.json", "utf-8");
-    const games = JSON.parse(data);
-
-    res.status(200).json(games);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to load games" });
-  }
+export default function handler(req, res) {
+  res.status(200).json([
+    { id: 1, name: "Test Game" }
+  ]);
 }

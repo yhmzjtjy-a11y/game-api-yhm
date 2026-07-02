5b7c116e334a8247c1976a12a9e1ba3d39127041
yhmzjtjy-a11y <yhmzjtjy@qq.com>
2026-07-02 14:11:42 +0800

    Update games.js

--- api/games.js
+++ api/games.js
@@ -1,5 +1,7 @@
 export default function handler(req, res) {
   res.status(200).json([
-    { id: 1, name: "Cyberpunk 2077" }
+    { id: 1, name: "Cyberpunk 2077", rating: 4.2 },
+    { id: 2, name: "Elden Ring", rating: 4.8 },
+    { id: 3, name: "Red Dead Redemption 2", rating: 4.9 }
   ]);
 }
